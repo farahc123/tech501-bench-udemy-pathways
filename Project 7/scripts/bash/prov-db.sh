5871dd28537c25c74cfc16bad447dab144b40a6a
farah <farahx.x@hotmail.com>
2025-07-18 16:08:03 +0100

    added bash scripts

---
+++ Project 7/scripts/bash/prov-db.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Define log file
+LOG_FILE="/farah_custom_data.log"
+
+# Redirect stdout and stderr to the log file
+exec > >(sudo tee -a "$LOG_FILE") 2>&1
+
+# get updates of packages with no user input
+echo "Updating package lists..."
+sudo apt update -y
+
+# downloads and installs the latest updates for packages
+echo "Upgrading installed packages..."
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
+
+# installs gnupg and curl
+echo "Installing gnupg and curl..."
+sudo DEBIAN_FRONTEND=noninteractive apt install gnupg curl -y
+
+# downloads gpg key
+echo "Downloading and adding MongoDB GPG key..."
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+sudo gpg --batch --yes -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+
+# creates file list
+echo "Adding MongoDB repository..."
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# updates packages again
+echo "Updating package lists again..."
+sudo apt update -y
+
+# installs MongoDB components
+echo "Installing MongoDB 7.0.6 components..."
+sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
+
+# changes the bindIP default setting
+echo "Updating MongoDB bindIp setting to listen to all sources..."
+sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
+
+# enabling mongod to start on boot, starts it
+echo "Enabling MongoDB service..."
+sudo systemctl enable mongod
+
+# checking if mongod is enabled
+echo "Checking if MongoDB service is enabled..."
+sudo systemctl is-enabled mongod
+
+# restarting mongodb to save new enabled setting
+echo "Restarting MongoDB service..."
+sudo systemctl restart mongod
+
+echo "Script execution completed. Logs stored in $LOG_FILE"
---
+++ Project 7/scripts/bash/run-app-only.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LOG_FILE="/farah_running_app_only.log"
+
+# Use sudo to write to the log file
+exec > >(sudo tee -a "$LOG_FILE") 2>&1
+
+echo "Changing directory to application folder..."
+cd /repo/nodejs20-sparta-test-app/app
+
+echo "Installing npm dependencies..."
+sudo npm install
+
+# Changed from export DB_HOST=mongodb://172.31.48.148:27017/posts to export DB_HOST=mongodb://10.0.3.4:27017/posts when I switched to Azure
+echo "Setting up database connection..."
+export DB_HOST=mongodb://10.0.3.4:27017/posts
+
+echo "Seeding the database..."
+node seeds/seed.js
+
+echo "Stopping the application using PM2..."
+pm2 delete app.js
+
+echo "Starting the application using PM2..."
+pm2 start app.js
+
+echo "Script execution completed. Logs stored in $LOG_FILE"
