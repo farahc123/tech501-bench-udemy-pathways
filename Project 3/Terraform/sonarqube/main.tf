bd290a02f968002ce1c86746d4f29ff28ef2cddd
farah <farahx.x@hotmail.com>
2025-08-01 19:29:07 +0100

    update

--- Project 3/Terraform/sonarqube/main.tf
+++ Project 3/Terraform/sonarqube/main.tf
@@ -1,17 +1,31 @@
-resource "google_compute_instance" "db_instance" {
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+data "google_compute_image" "ubuntu" {
+  family  = "ubuntu-2404-lts-amd64"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_instance" "sonarqube_instance" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = var.image
+      image = data.google_compute_image.ubuntu.self_link
     }
   }
 
   network_interface {
-    access_config {} # gives it a public IP
+    access_config {} # assigns a public IP
+    network = "default"
   }
+
+  metadata_startup_script = file("sonarqube-setup.sh")
 
   tags = [var.vm_tag]
 }
